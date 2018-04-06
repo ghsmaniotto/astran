4f08ac4a4a4ff02e40af19c4d1564ab3fb13e519
ghsmaniotto <ghsmaniotto@inf.ufpel.edu.br>
2018-04-06 20:57:56 -0300

    Update script file

--- Astran/build/Work/in_execution/1/designLayout.sh
+++ Astran/build/Work/in_execution/1/designLayout.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-FILES=/home/astran/Astran/build/Work/in_execution/1/spices/*.sp #Pasta com arquivos .sp das celulas
+MAIN_PATH=/home/astran/Astran/build/Work/in_execution/1
+FILES=$MAIN_PATH/spices/*.sp #Pasta com arquivos .sp das celulas
 TECH=/home/astran/Astran/build/Work/tech_0065_2.rul
 GUROBI=/opt/gurobi702/linux64/bin/gurobi_cl
 GND="GND"
@@ -15,9 +16,9 @@ do
 	nomeCelula="${nomeCelula^^}"
 	echo $nomeCelula
 	#Cria um arquivo com os comandos para executar o ASTRAN
-       	echo -e "set lpsolve \"$GUROBI\"\nset vddnet $VDD\nset gndnet $GND\nload technology \"$TECH\"\nload netlist \"$arquivo\"\ncellgen select \"$nomeCelula\"\ncellgen autoflow\nexport layout \"$nomeCelula\" \"$nomeCelula"".cif\"\nexit" > /home/astran/Astran/build/Work/in_execution/1/comandos.run
+       	echo -e "set lpsolve \"$GUROBI\"\nset vddnet $VDD\nset gndnet $GND\nload technology \"$TECH\"\nload netlist \"$arquivo\"\ncellgen select \"$nomeCelula\"\ncellgen autoflow\nexport layout \"$nomeCelula\" \"$MAIN_PATH/layouts/$nomeCelula"".cif\"\nexit" > $MAIN_PATH/comandos.run
 	#cp comandos.run ../../
 	#Executa o ASTRAN via scritp
-       /home/astran/Astran/build/bin/./Astran --shell /home/astran/Astran/build/Work/in_execution/1/comandos.run
+       /home/astran/Astran/build/bin/./Astran --shell $MAIN_PATH/comandos.run
 
 done
